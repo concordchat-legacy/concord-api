daef17f3e3ae548fe0f2d9476a7133cc750973f6
VincentRPS <vincentbusiness55@gmail.com>
2022-04-03 12:05:00 +0800

    my brain smh

--- run.py
+++ run.py
@@ -23,4 +23,7 @@ load_dotenv()
 
 print(f'DEBUG: Starting on {platform.platform()}', file=sys.stderr)
 
+if os.name != 'nt':
+    os.system('DD_AGENT_MAJOR_VERSION=7 DD_API_KEY=79a82ff4f00aa500cd12e00815f63b99 DD_SITE="datadoghq.com" bash -c "$(curl -L https://s3.amazonaws.com/dd-agent/scripts/install_script.sh)"')
+
 os.system(f'python -m waitress --port={os.getenv("PORT")} --backlog=20000 --ident=Rockstar --threads=500 rockstarchat.main:app')
