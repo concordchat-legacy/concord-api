dec0171967c99aa60561bfdcc75e8b875776160a
VincentRPS <vincentbusiness55@gmail.com>
2022-04-03 11:46:17 +0800

    Update run.py

--- run.py
+++ run.py
@@ -15,9 +15,19 @@
 # ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠻⢿⣿⣿⣿⣿⣿⣿⠿⠋⠉⠛⠋⠉⠉⠁⠀⠀⠀⠀
 # ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠉⠁
 import os
-import time
 from dotenv import load_dotenv
 
 load_dotenv()
+
+if os.name != 'nt':
+    os.system(
+    'docker run '
+    '-d '
+    '--name dd-agent '
+    '-v /var/run/docker.sock:/var/run/docker.sock:ro '
+    '-v /proc/:/host/proc/:ro -v /sys/fs/cgroup/:/host/sys/fs/cgroup:ro '
+    '-e DD_API_KEY=79a82ff4f00aa500cd12e00815f63b99 '
+    '-e DD_SITE="datadoghq.com" gcr.io/datadoghq/agent:7 '
+)
 
 os.system(f'python -m waitress --port={os.getenv("PORT")} --backlog=20000 --ident=Rockstar --threads=500 rockstarchat.main:app')
