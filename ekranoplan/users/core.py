3c42d60704330a8cc10a86f336fd8573abc89ebd
VincentRPS <vincentbusiness55@gmail.com>
2022-04-30 17:41:57 +0800

    fix: stringify on create as well

--- ekranoplan/users/core.py
+++ ekranoplan/users/core.py
@@ -71,10 +71,10 @@ class CoreUsers(Controller):
             raise BadData()
 
         if data.get('avatar'):
-            pfp_id = upload_image(data['avatar'], 'users')
+            pfp_id = upload_image(str(data['avatar']), 'users')
 
         if data.get('banner'):
-            banner_id = upload_image(data['banner'], 'users')
+            banner_id = upload_image(str(data['banner']), 'users')
 
         user: User = User.create(
             id=factory().formulate(),
